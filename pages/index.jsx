c9517c2343b291b1e9e742705106a9f7e08958aa
BrandonRomano <BrandonRRomano@gmail.com>
2020-03-31 19:36:33 -0700

    Initial commit (stubbing)

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Button from '../components/button'
+
+export default function Index() {
+  return (
+    <div className="g-wow">
+      <Button />
+      <p>Hello Next.js</p>
+    </div>
+  );
+}
