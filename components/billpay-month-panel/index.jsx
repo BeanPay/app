335e7f29c80f39ef5798cea0c8952f1dad6597d5
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-21 21:06:57 -0700

    Adjust BillpayMonth panel to only have the month

--- components/billpay-month-panel/index.jsx
+++ components/billpay-month-panel/index.jsx
@@ -1,16 +1,14 @@
 import styles from './BillpayMonthPanel.module.css'
 import DashboardPanel from '../../design-system/dashboard-panel'
 import BillpayCalendarMonth from '../billpay-calendar-month'
-import BillpayList from '../billpay-list'
 
 import BillpayCalendarDay from '../billpay-calendar-day'
 
 export default function BillpayMonthPanel({month, year, bills}) {
   const monthName = new Date(year, month-1, 1).toLocaleString('default', { month: 'long' });
   return (
-    <DashboardPanel className={styles.billpayMonthPanel} title={`${monthName} Bills`} minRowWeight={50}>
+    <DashboardPanel className={styles.billpayMonthPanel} title={monthName} minRowWeight={50}>
       <BillpayCalendarMonth className={styles.calendar} month={month} year={year} bills={bills} />
-      <BillpayList className={styles.list} bills={bills} />
     </DashboardPanel>
   )
 }
--- pages/bills/index.jsx
+++ pages/bills/index.jsx
@@ -45,6 +45,47 @@ export default function Bills() {
           },
         ]}
       />
+      <BillpayMonthPanel
+        month={5}
+        year={2020}
+        bills={[
+          {
+            name: 'Rent',
+            billpayURL: 'https://www.rentpayment.com/pay/login.html',
+            dueDate: 1,
+            totalDue: 1050,
+            paid: false,
+          },
+          {
+            name: 'Waste Management',
+            billpayURL: 'https://www.wm.com/us/en/mywm/my-payment/verify',
+            dueDate: 5,
+            totalDue: 24,
+            paid: false,
+          },
+          {
+            name: 'PSE&G',
+            billpayURL: 'https://nj.pseg.com/',
+            dueDate: 15,
+            totalDue: 130,
+            paid: false,
+          },
+          {
+            name: 'Chase Sapphire',
+            billpayURL: 'https://nj.pseg.com/',
+            dueDate: 23,
+            totalDue: 421,
+            paid: false,
+          },
+          {
+            name: 'Water Bill',
+            billpayURL: 'https://wss.amwater.com/selfservice-web/login.do',
+            dueDate: 27,
+            totalDue: 37,
+            paid: false,
+          },
+        ]}
+      />
     </DashboardLayout>
   );
 }
