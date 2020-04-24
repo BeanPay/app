82bf00a9cf4e7da70225ef4990202d03e11483e7
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-24 01:31:43 -0700

    Style popup content

--- components/popper-example/index.jsx
+++ components/popper-example/index.jsx
@@ -1,3 +1,4 @@
+import BillpayList from '../billpay-list'
 import moduleStyles from './PopperExample.module.css'
 import PopupPanel from '../popup-panel'
 import { usePopper } from 'react-popper';
@@ -33,7 +34,32 @@ export default function Example() {
             setPopupOpen(false);
           }}
         >
-          <p>Hello world :)</p>
+          <BillpayList
+            isPast={true}
+            bills={[
+              {
+                name: 'Rent',
+                billpayURL: 'https://www.rentpayment.com/pay/login.html',
+                dueDate: 1,
+                totalDue: 1050,
+                paid: true,
+              },
+              {
+                name: 'Waste Management',
+                billpayURL: 'https://www.wm.com/us/en/mywm/my-payment/verify',
+                dueDate: 5,
+                totalDue: 24,
+                paid: false,
+              },
+              {
+                name: 'PSE&G',
+                billpayURL: 'https://nj.pseg.com/',
+                dueDate: 15,
+                totalDue: 130,
+                paid: false,
+              },
+            ]}
+          />
         </PopupPanel>
       )}
     </>
