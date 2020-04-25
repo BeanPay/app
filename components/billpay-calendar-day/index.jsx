68d8a64b63fc0b9e7b846c459e6f28f4f4e0bdd0
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-24 23:31:30 -0700

    Feed in real data to popups

--- components/billpay-calendar-day/index.jsx
+++ components/billpay-calendar-day/index.jsx
@@ -1,21 +1,24 @@
+import { usePopper } from 'react-popper';
+import React, { useState } from 'react';
 import classNames from 'classnames'
+import SVG from 'react-inlinesvg'
 import withOrdinalSuffix from '../../util/with-ordinal-suffix'
 import CheckIcon from './check.svg'
-import SVG from 'react-inlinesvg'
 import dayStyles from './BillpayCalendarDay.module.css'
 import BillpayList from '../billpay-list'
 import PopupPanel from '../popup-panel'
-import { usePopper } from 'react-popper';
-import React, { useState } from 'react';
 
 export default function BillpayCalendarDay({month, year, day, bills}) {
   // Determine the dates relative reference to todays actual date
   var today = new Date();
-  today.setHours(0,0,0,0)
+  today.setHours(0,0,0,0);
   today = today.getTime();
   const isPast = new Date(year, month - 1, day).getTime() < today;
   const isToday = new Date(year, month - 1, day).getTime() == today;
   const isOffset = day === 0;
+
+  // Get the month name for the modal
+  const monthName = new Date(year, month - 1, day).toLocaleString('default', { month: 'long' });
 
   // Determine if & what we owe
   var totalDue = 0;
@@ -27,7 +30,7 @@ export default function BillpayCalendarDay({month, year, day, bills}) {
         billStatus = 'due';
       } else {
         if(billStatus !== 'due') {
-          billStatus = 'paid'
+          billStatus = 'paid';
         }
       }
     }
@@ -70,7 +73,7 @@ export default function BillpayCalendarDay({month, year, day, bills}) {
 
       { popupOpen && (
         <PopupPanel
-          title="April 23rd"
+          title={`${monthName} ${withOrdinalSuffix(day)}`}
           ref={setPopperElement}
           style={styles.popper}
           popperAttributes={attributes.popper}
@@ -78,32 +81,7 @@ export default function BillpayCalendarDay({month, year, day, bills}) {
             setPopupOpen(false);
           }}
         >
-          <BillpayList
-            isPast={true}
-            bills={[
-              {
-                name: 'Rent',
-                billpayURL: 'https://www.rentpayment.com/pay/login.html',
-                dueDate: 1,
-                totalDue: 1050,
-                paid: true,
-              },
-              {
-                name: 'Waste Management',
-                billpayURL: 'https://www.wm.com/us/en/mywm/my-payment/verify',
-                dueDate: 5,
-                totalDue: 24,
-                paid: false,
-              },
-              {
-                name: 'PSE&G',
-                billpayURL: 'https://nj.pseg.com/',
-                dueDate: 15,
-                totalDue: 130,
-                paid: false,
-              },
-            ]}
-          />
+          <BillpayList isPast={isPast} bills={bills} />
         </PopupPanel>
       )}
     </>
