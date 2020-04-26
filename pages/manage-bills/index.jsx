36657ffa7208bf24457fe309b85472a6ae106c60
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-25 19:34:47 -0700

    Cleanup settings

--- pages/manage-bills/index.jsx
+++ pages/manage-bills/index.jsx
@@ -1,27 +1,11 @@
 import DashboardLayout from '../../layouts/dashboard-layout'
 import DashboardPanel from '../../design-system/dashboard-panel'
 
-import PopupMenu from '../../components/popup-menu'
-import { faExternalLinkAlt, faReceipt } from '@fortawesome/free-solid-svg-icons'
-
 export default function Settings() {
   return (
     <DashboardLayout pageTitle="Manage Bills">
       <DashboardPanel title="Some Settings" minRowWeight={100}>
-        <PopupMenu
-          items={[
-            {
-              text: "Pay Bill",
-              onClick: () => console.log("Pay Bill Clicked"),
-              icon: faExternalLinkAlt,
-            },
-            {
-              text: "Mark as Paid",
-              onClick: () => console.log("Mark as Paid Clicked"),
-              icon: faReceipt,
-            }
-          ]}
-        />
+        <h1>Hello settings</h1>
       </DashboardPanel>
     </DashboardLayout>
   );
