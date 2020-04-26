cc672bf81337d847162f1e3d511e8f0339bf4370
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-25 22:57:22 -0700

    Adds Share Card image

--- layouts/base-layout/index.jsx
+++ layouts/base-layout/index.jsx
@@ -5,6 +5,7 @@ export default function BaseLayout({className, pageTitle, children}) {
     <>
       <Head>
         <title>BeanPay - {pageTitle}</title>
+        <meta property="og:image" content="/logos/share-card.png" />
         <link rel="icon" href="/logos/logo.svg" />
       </Head>
       <div className={className}>
