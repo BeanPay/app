56741bbe87adb60167bda2371dd04b6acd52df6c
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-25 21:54:03 -0700

    Rename BillpayCalendarMonth to CalendarMonth

--- components/billpay-month-panel/index.jsx
+++ components/billpay-month-panel/index.jsx
@@ -1,14 +1,12 @@
 import styles from './BillpayMonthPanel.module.css'
 import DashboardPanel from '../../design-system/dashboard-panel'
-import BillpayCalendarMonth from '../billpay-calendar-month'
-
-import BillpayCalendarDay from '../billpay-calendar-day'
+import CalendarMonth from '../calendar-month'
 
 export default function BillpayMonthPanel({month, year, bills}) {
   const monthName = new Date(year, month-1, 1).toLocaleString('default', { month: 'long' });
   return (
     <DashboardPanel className={styles.billpayMonthPanel} title={monthName} minRowWeight={50}>
-      <BillpayCalendarMonth className={styles.calendar} month={month} year={year} bills={bills} />
+      <CalendarMonth className={styles.calendar} month={month} year={year} bills={bills} />
     </DashboardPanel>
   )
 }
--- components/calendar-month/index.jsx
+++ components/calendar-month/index.jsx
@@ -1,12 +1,12 @@
 import classNames from 'classnames'
-import styles from './BillpayCalendarMonth.module.css'
+import styles from './CalendarMonth.module.css'
 import BillpayCalendarDay from '../billpay-calendar-day'
 
-export default function BillpayCalendarMonth({className, month, year, bills}) {
+export default function CalendarMonth({className, month, year, bills}) {
   const firstWeekOffset = new Date(year + "-" + month + "-01").getDay();
   const numDaysInMonth = new Date(year, month, 0).getDate();
   return (
-    <div className={classNames(styles.billpayCalendarMonth, className)}>
+    <div className={classNames(styles.calendarMonth, className)}>
       {/* Plant all of the offset divs */}
       {Array(firstWeekOffset).fill().map((v, i)=>(
         <BillpayCalendarDay key={i} day={0} />
