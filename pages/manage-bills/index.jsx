8968072c014ac341020c63fe064a5ce8ae4d50f8
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-29 19:08:57 -0700

    Create simple section header component

--- pages/manage-bills/index.jsx
+++ pages/manage-bills/index.jsx
@@ -1,10 +1,12 @@
 import DashboardLayout from '../../layouts/dashboard-layout'
 import DashboardPanel from '../../design-system/dashboard-panel'
+import DashboardSectionHeader from '../../design-system/dashboard-section-header'
 
 export default function Settings() {
   return (
     <DashboardLayout pageTitle="Manage Bills">
-      <DashboardPanel title="Some Settings" minRowWeight={100}>
+      <DashboardSectionHeader title="Manage Bills" />
+      <DashboardPanel title="Some Settings" minRowWeight={50}>
         <h1>Hello settings</h1>
       </DashboardPanel>
     </DashboardLayout>
