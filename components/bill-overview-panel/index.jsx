875af401dba260af58174fe6b79d764178456897
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-29 19:15:03 -0700

    Stub bill overview panel

---
+++ components/bill-overview-panel/index.jsx
@@ -0,0 +1,7 @@
+import DashboardPanel from '../../design-system/dashboard-panel'
+
+export default function BillOverviewPanel() {
+  return (
+    <DashboardPanel title="Mortgage" description="$1050 due monthly on the 1st." minRowWeight={33}/>
+  )
+}
--- pages/manage-bills/index.jsx
+++ pages/manage-bills/index.jsx
@@ -1,14 +1,14 @@
 import DashboardLayout from '../../layouts/dashboard-layout'
-import DashboardPanel from '../../design-system/dashboard-panel'
 import DashboardSectionHeader from '../../design-system/dashboard-section-header'
+import BillOverviewPanel from '../../components/bill-overview-panel'
 
 export default function Settings() {
   return (
     <DashboardLayout pageTitle="Manage Bills">
       <DashboardSectionHeader title="Manage Bills" />
-      <DashboardPanel title="Some Settings" minRowWeight={50}>
-        <h1>Hello settings</h1>
-      </DashboardPanel>
+      <BillOverviewPanel />
+      <BillOverviewPanel />
+      <BillOverviewPanel />
     </DashboardLayout>
   );
 }
