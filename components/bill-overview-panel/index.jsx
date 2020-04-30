ba619ab6727e03ca95f2c63bdaa10134ad9a5b85
BrandonRomano <BrandonRRomano@gmail.com>
2020-04-29 19:23:56 -0700

    Apply min-width to BillOverviewPanel

--- components/bill-overview-panel/index.jsx
+++ components/bill-overview-panel/index.jsx
@@ -1,7 +1,13 @@
+import styles from './BillOverviewPanel.module.css'
 import DashboardPanel from '../../design-system/dashboard-panel'
 
 export default function BillOverviewPanel() {
   return (
-    <DashboardPanel title="Mortgage" description="$1050 due monthly on the 1st." minRowWeight={33}/>
+    <DashboardPanel
+      className={styles.billOverviewPanel}
+      title="Mortgage"
+      description="$1050 due monthly on the 1st."
+      minRowWeight={33}
+    />
   )
 }
