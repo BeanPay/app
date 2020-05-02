f533823f2ff360d5a4f2ce128cee666e4abd0920
BrandonRomano <BrandonRRomano@gmail.com>
2020-05-02 01:35:01 -0700

    Adds in example modal

--- components/bill-settings-list/index.jsx
+++ components/bill-settings-list/index.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import BillSettingsPanel from './bill-settings-panel';
 import AddNewBill from './add-new-bill';
+import FullscreenModal from '../../design-system/fullscreen-modal'
 
 export default function BillSettingsList({ bills }) {
+  const [newBillModalOpen, setNewBillModalOpen] = useState(false);
   return (
     <>
       {bills.map(bill => (
         <BillSettingsPanel bill={bill} />
       ))}
-        <AddNewBill onClick={() => console.log("New Bill Click")}/>
+      <AddNewBill onClick={() => setNewBillModalOpen(true)}/>
+
+      <FullscreenModal
+        title="New Bill"
+        isOpen={newBillModalOpen}
+        onClose={() => setNewBillModalOpen(false)}
+      >
+        <p>This is the new bill form.</p>
+      </FullscreenModal>
     </>
   )
 }
