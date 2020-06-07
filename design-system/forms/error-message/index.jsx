82a5d968e5623565c4e2c661d4eec59635166eca
BrandonRomano <BrandonRRomano@gmail.com>
2020-06-07 16:06:25 -0700

    Adds support for error messages on forms

---
+++ design-system/forms/error-message/index.jsx
@@ -0,0 +1,9 @@
+import styles from './ErrorMessage.module.css';
+
+export default function ErrorMessage({ message }) {
+  return (
+    <div className={styles.errorMessage}>
+      <p>{message}</p>
+    </div>
+  )
+}
