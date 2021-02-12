6e50dcdbd9b28e08c141e8bc68bf643116ee963d
Brandon Romano <BrandonRRomano@gmail.com>
2021-02-11 20:28:53 -0800

    Nav dropdown adjustments

--- design-system/topnav/nav-dropdown/index.jsx
+++ design-system/topnav/nav-dropdown/index.jsx
@@ -3,13 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classNames from 'classnames'
 
 export default function NavDropdown({ className, navItems }){
-
   return(
     <ul className={classNames(className, styles.navDropdown)}>
       {
         navItems.map((navItem) => {
           return(
-            <li className={styles.navItem} key={navItem.name} onClick={navItem.onClick}>
+            <li key={navItem.name} onClick={navItem.onClick}>
               <FontAwesomeIcon icon={navItem.icon} />
               <h4>{navItem.name}</h4>
             </li>
